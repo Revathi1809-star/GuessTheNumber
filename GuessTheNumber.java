013ab63919350202bd42669104e8294f2fbcdf9f
Revathi1809-star <prevathiprevathi6@gmail.com>
2024-10-10 22:36:55 +0530

    Add files via upload

---
+++ GuessTheNumber.java
@@ -0,0 +1,36 @@
+import java.util.Random;
+import java.util.Scanner;
+
+public class GuessTheNumber {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        Random random = new Random();
+        
+        int numberToGuess = random.nextInt(100) + 1; // Random number between 1 and 100
+        int numberOfTries = 0;
+        boolean hasGuessedCorrectly = false;
+
+        System.out.println("Welcome to 'Guess the Number'!");
+        System.out.println("I have selected a number between 1 and 100. Can you guess it?");
+
+        while (!hasGuessedCorrectly) {
+            System.out.print("Enter your guess: ");
+            int userGuess = scanner.nextInt();
+            numberOfTries++;
+
+            if (userGuess < 1 || userGuess > 100) {
+                System.out.println("Please guess a number between 1 and 100.");
+            } else if (userGuess < numberToGuess) {
+                System.out.println("Too low! Try again.");
+            } else if (userGuess > numberToGuess) {
+                System.out.println("Too high! Try again.");
+            } else {
+                hasGuessedCorrectly = true;
+                System.out.printf("Congratulations! You've guessed the number %d in %d tries.%n", numberToGuess, numberOfTries);
+            }
+        }
+
+        scanner.close();
+    }
+}
+
